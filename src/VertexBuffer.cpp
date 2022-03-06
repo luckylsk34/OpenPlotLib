18080d6790ea04c29bef5aa89544793799278062
Lakkam Sai Krishna Reddy <lakkam.saikrishnareddy@gmail.com>
2022-03-06 19:48:17 +0530

    Added VertexBuffer class

---
+++ src/VertexBuffer.cpp
@@ -0,0 +1,38 @@
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "VertexBuffer.h"
+
+VertexBuffer::VertexBuffer()
+{
+	glGenBuffers(1, &buffer_id);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+}
+
+VertexBuffer::VertexBuffer(const void* data, unsigned int size)
+{
+	glGenBuffers(1, &buffer_id);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+	send_data(data, size);
+}
+
+VertexBuffer::~VertexBuffer()
+{
+	glDeleteBuffers(1, &buffer_id);
+}
+
+void VertexBuffer::bind() const
+{
+	glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+}
+
+void VertexBuffer::unbind() const
+{
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VertexBuffer::send_data(const void* data, unsigned int size) const
+{
+	glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+}
---
+++ src/VertexBuffer.h
@@ -0,0 +1,30 @@
+class VertexBuffer
+{
+private:
+	unsigned int buffer_id;
+public:
+	VertexBuffer();
+	VertexBuffer(const void* data, unsigned int size);
+	~VertexBuffer();
+
+	void bind() const;
+	void unbind() const;
+	void send_data(const void* data, unsigned int size) const;
+};
+
+// To be implemented later.
+// class VariableVertexBuffer : public VertexBuffer
+// {
+// 	void **data;
+// 	void *sizes;
+// 	int num_data = 0;
+// public:
+// 	VariableVertexBuffer();
+// 	~VariableVertexBuffer();
+
+// 	void bind() const;
+// 	void unbind() const;
+// 	void add_data(const void* data, unsigned int size) const;
+// 	void send_data() const;
+// };
+
